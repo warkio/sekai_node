4dc993bc10976f6f1c1e2976e4c193c64a2da158
Aztic <eien@wark.io>
2019-02-13 06:19:20 +0100

    fixed 'create_model' script var name

--- create_model.js
+++ create_model.js
@@ -2,21 +2,21 @@ const fs = require('fs');
 const path = require('path');
 const startCase = require('lodash.startcase');
 const snakeCase = require('lodash.snakecase');
-const categoryName = startCase(process.argv[2]).replace(" ", "");
+const modelName = startCase(process.argv[2]).replace(" ", "");
 const tableName = process.argv[3];
 
 const modelPath = path.join(__dirname, "models", snakeCase(process.argv[2])+".js");
 
 const template = `const { BaseModel } = require('./base_model');
 
-class ${categoryName} extends BaseModel {
+class ${modelName} extends BaseModel {
     constructor(){
         super();
-        this.tableName = ${categoryName}.tableName;
+        this.tableName = ${modelName}.tableName;
     }
 }
-${categoryName}.tableName = "${tableName}";
-module.exports = { ${categoryName} };
+${modelName}.tableName = "${tableName}";
+module.exports = { ${modelName} };
 `
 
 fs.writeFileSync(modelPath, template);
